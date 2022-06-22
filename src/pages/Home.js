2e767f2cc7cc707909e1db19d51cd02437429fa4
samyaahsan <96796200+samyaahsan@users.noreply.github.com>
2022-06-21 23:35:52 -0400

    working version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,12 +21,28 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
+    let new_list = [...this.state.todos];
     // Update the local state with the new array.
+    if(!new_list.find(x => x.content === todo.content)){
+      new_list = [...this.state.todos, todo];
+    }
+
     this.setState({
       todos: new_list,
     });
+
+
   };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  }; 
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +52,9 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        
       </div>
     );
   }
